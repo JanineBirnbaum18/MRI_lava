2e0c67f71cbbda7acb9921570b049b7f6fa414fb
Janine Birnbaum <30421133+JanineBirnbaum18@users.noreply.github.com>
2021-04-28 13:30:30 -0400

    Add load_dicom.m
    Reads a series of dicom images and creates an animation

---
+++ Load_dicom.m
@@ -0,0 +1,46 @@
+% Load dicom files from MRI scans
+
+% Signal intensity
+path_to_download = 'path\to\diacom\folder';
+path_mag = 'BEAT_FQ2';
+comp_mag = dir([path_to_download '\' path_mag '\']);
+files_mag = dir([path_to_download '\' path_mag '\' comp_mag(3).name]);
+
+path_vel = 'BEAT_FQ2_P';
+comp_vel = dir([path_to_download '\' path_vel '\']);
+files_vel = dir([path_to_download '\' path_vel '\' comp_vel(3).name]);
+
+close all
+
+figure('units','normalized','outerposition',[0 0 1 1]);
+clf;
+
+F(length(files_mag)-2) = struct('cdata',[],'colormap',[]);
+
+for i = 1:(length(files_mag)-2)
+    info_mag = dicominfo([path_to_download '\' path_mag '\' comp_mag(3).name '\' files_mag(i+2).name]);
+    mag = dicomread(info_mag);
+    mag = (double(mag'))/double((2^info.BitDepth));
+    subplot(2,1,1)
+    imshow(mag,[])
+    caxis([0 0.1])
+    colorbar
+    title('Signal Intensity')
+    
+    info_vel = dicominfo([path_to_download '\' path_vel '\' comp_vel(3).name '\' files_vel(i+2).name]);
+    vel = dicomread(info_vel);
+    vel = -4*(double(vel')-double(2^(info.BitDepth-1)))/double((2^info.BitDepth-1));
+    vel(mag<0.005) = nan;
+    subplot(2,1,2)
+    imshow(vel,[])
+    caxis([-1,4])
+    colorbar
+    title('Velocity (cm/s)')
+    
+    F(i) = getframe(gcf);
+end
+
+v = VideoWriter('output_video_name');
+open(v);
+writeVideo(v,F);
+close(v);
